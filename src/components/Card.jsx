c938dcc49ebaee3741f14a2b707d9969f7ab6cbc
EMMMABK <adilbikiev2@gmail.com>
2025-02-23 20:37:54 +0600

    add Card display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,21 @@ const Card = ({data}) => {
     console.log(data);
     
   return (
-    <div>
-      
+    <div className='cardContainer'>
+      {data.map((curItem, index) => {
+        return(
+            <div className="card">
+                <img src={curItem.urlToImage} alt="" />
+                <div className="cardContent">
+                    <a className='cardContent_title'>{curItem.title}</a>
+                    <p className='cardContent_subtitle'>{curItem.description}</p>
+                    <button onClick={() => {
+                      curItem.url
+                    }} className='ReadBtn'>Read More</button>
+                </div>
+            </div>
+        )
+      })}
     </div>
   )
 }
