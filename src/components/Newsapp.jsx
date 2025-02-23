9f9f10349f0c241e0b29e8fd91026dae0620aa77
EMMMABK <adilbikiev2@gmail.com>
2025-02-23 20:04:22 +0600

    add Templeate 2

--- src/components/Newsapp.jsx
+++ src/components/Newsapp.jsx
@@ -4,16 +4,28 @@ const Newsapp = () => {
   return (
     <div>
       <nav>
-        <div className="">Trend News</div>
+        <div className="trend_news">Trend News</div>
         <ul>
-            <a href="">All News</a>
-            <a href="">Trending</a>
+            <a href="#">All News</a>
+            <a href="#">Trending</a>
         </ul>
         <div className="searchBar">
             <input type="text" placeholder='Search News'/>
             <button>Search</button>
         </div>
       </nav>
+
+      <div className="categoryBtn">
+        <button>Sports</button>
+        <button>Politics</button>
+        <button>Entertainment</button>
+        <button>Health</button>
+        <button>Fitness</button>
+      </div>
+
+      <div className="">
+
+      </div>
     </div>
   )
 }
