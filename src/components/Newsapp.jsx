a4be9830f43bf380f396d9fce32943808d2ebc3a
EMMMABK <adilbikiev2@gmail.com>
2025-02-23 20:02:15 +0600

    add Template

--- src/components/Newsapp.jsx
+++ src/components/Newsapp.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Newsapp = () => {
   return (
     <div>
-      
+      <nav>
+        <div className="">Trend News</div>
+        <ul>
+            <a href="">All News</a>
+            <a href="">Trending</a>
+        </ul>
+        <div className="searchBar">
+            <input type="text" placeholder='Search News'/>
+            <button>Search</button>
+        </div>
+      </nav>
     </div>
   )
 }
